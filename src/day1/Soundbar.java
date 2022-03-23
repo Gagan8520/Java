4427b0f51e43e87921158f5cab005493cbf480fb
Gagan Dangol <gagandangol@Gagans-MBP.attlocal.net>
2022-03-23 00:31:22 -0500

    day2 assignment

---
+++ src/day1/Soundbar.java
@@ -0,0 +1,19 @@
+package day1;
+
+public class Soundbar {
+
+    //states(variables, constant)
+    //dimension
+    //weight
+    //length
+    //color
+    //power
+
+    //Behaviour(methods or functions)
+    //playing music
+    //connecting with external devices
+    //increasing volume
+    //decreasing volume
+    //changing source of output
+    //changing source of input
+}
---
+++ src/day2/Facebookloginpage.java
@@ -0,0 +1,20 @@
+package day2;
+
+public class Facebookloginpage {
+
+    //states
+    //facebook logo
+    //login
+    //email or phone number
+    //password
+    //forget password
+    //create new account
+    //create a page
+    //language selection
+
+    //behavior
+    //logging in with the help of username and password
+    //creating a page
+    //creating new account
+    //recovering the password
+}
