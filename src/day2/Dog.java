102993155e3c4fcbaf02dafabe1c7568b241c162
Gagan Dangol <gagandangol@Gagans-MacBook-Pro.local>
2022-03-23 21:11:24 -0500

    Correction

---
+++ src/day2/Dog.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Dog {
+
+    //states
+    //color
+    //weight
+    //height
+    //age
+    //breed
+    //gender
+    //eyeColor
+    //name
+
+    //behavior
+    //playing
+    //barking
+    //biting
+    //eatingFood
+    //sleep
+}
