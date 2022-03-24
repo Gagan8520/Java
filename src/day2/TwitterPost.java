fdf2d44d2e871e165d4914795a68eba9ca8ca483
Gagan Dangol <gagandangol@Gagans-MacBook-Pro.local>
2022-03-23 21:43:34 -0500

    Assignment

---
+++ src/day2/TwitterPost.java
@@ -0,0 +1,28 @@
+package day2;
+
+public class TwitterPost {
+
+    //states
+    //caption
+    String caption;
+    //username
+    String username;
+    //name
+    String name;
+    //attachmentsIfAny
+    String attachmentsIfAny;
+    //numberOfRetweet
+    int numberOfRetweet;
+    //numberOfLoveReact
+    int numberOfLoveReact;
+    //numberOfComment
+    int numberOfComment;
+    //shareTweet
+    String shareTweet;
+
+    //behavior
+    //sharingTweet
+    //retweeting
+    //goingToTheUploader-profile
+    //commenting
+}
